fbea2fc5b54bceab65c12703d25b317e499fa3a2
greenapple18 <114010133+greenapple18@users.noreply.github.com>
2023-03-21 10:52:06 -0400

    Add cinemachine tracking

--- Platformer/Assets/Codes/UserControl.cs
+++ Platformer/Assets/Codes/UserControl.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class UserControl : MonoBehaviour
 {
+    // Player Variables
     private Rigidbody2D _rigidbody;
     public int playerJumpVal = 10;
     public int playerSpeed = 5;
+    
 
+    // Variables to manage jumping
     public Transform playerShoes;
     public LayerMask terrain;
     bool onTerrain = false;
+
+    //Start
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    // Fixed Update
     void FixedUpdate() {
         float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
         _rigidbody.velocity = new Vector2(horizontalMovement, _rigidbody.velocity.y);
-        float verticalLocation = Input.GetAxis("Vertical") * playerSpeed;
     }
+
+    // Update
     void Update()
     {
-        onTerrain = Physics2D.OverlapCircle(playerShoes.position,.3f, terrain);
+        onTerrain = Physics2D.OverlapCircle(playerShoes.position, .3f, terrain);
         if(onTerrain && Input.GetButton("Jump")){
             _rigidbody.AddForce(new Vector2(0,playerJumpVal));
         }
