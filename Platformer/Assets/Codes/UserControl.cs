d9f74023ef4f5381848db55d71892c03af5f0adb
greenapple18 <114010133+greenapple18@users.noreply.github.com>
2023-03-21 10:05:13 -0400

    Add colliders and change player movement
    Add user control like walking and jumping

--- Platformer/Assets/Codes/UserControl.cs
+++ Platformer/Assets/Codes/UserControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class UserControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rigidbody;
+    public int playerJumpVal = 10;
+    public int playerSpeed = 5;
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void FixedUpdate() {
+        float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
+        _rigidbody.velocity = new Vector2(horizontalMovement, _rigidbody.velocity.y);
+        float verticalLocation = Input.GetAxis("Vertical") * playerSpeed;
+    }
     void Update()
     {
-        
+        if(Input.GetButton("Jump")){
+            _rigidbody.AddForce(new Vector2(0,playerJumpVal));
+        }
     }
 }
