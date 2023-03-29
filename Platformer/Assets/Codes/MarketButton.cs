c6e21206eaeffeb8f5c5a53ede59839ed5b0b9c3
greenapple18 <114010133+greenapple18@users.noreply.github.com>
2023-03-29 14:56:19 -0400

    Update MarketButton.cs

--- Platformer/Assets/Codes/MarketButton.cs
+++ Platformer/Assets/Codes/MarketButton.cs
@@ -8,6 +8,7 @@ public class MarketButton : MonoBehaviour
 {
     public string level = "level 1";
     public void Accept(){
+        publicvar.numberCoins = -1;
         SceneManager.LoadScene(level);
     }
 
