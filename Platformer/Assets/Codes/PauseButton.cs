3cac47c45a08596e93dc7218e78131b9120d2476
hj2118 <hj2118@nyu.edu>
2023-04-06 15:30:00 -0400

    added hearts

---
+++ Platformer/Assets/Codes/PauseButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Home() {
+        #if !UNITY_WEBGL
+        Application.Quit();
+        #endif
+
+        #if UNITY_WEBGL
+        SceneManager.LoadScene("StartGame");
+        #endif
+    }
+}
